b59cc007f96ee3fe7a57c9e789b3457c8ef81737
kz <kaiyin@zhong.science>
2026-05-21 19:18:55 +0200

    colors fine tune

--- plasma/look-and-feel/com.github.vinceliuice.Layan-light/contents/layouts/org.kde.plasma.desktop-layout.js
+++ plasma/look-and-feel/com.github.vinceliuice.Layan-light/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -85,6 +85,38 @@ var layout = {
                             "immutability": "1"
                         },
                         "/Configuration": {
+                            "CurrentPreset": "org.kde.plasma.systemmonitor",
+                            "popupHeight": "400",
+                            "popupWidth": "560"
+                        },
+                        "/Configuration/Appearance": {
+                            "chartFace": "org.kde.ksysguard.barchart",
+                            "title": "Individueel gebruik van kernen"
+                        },
+                        "/Configuration/SensorColors": {
+                            "cpu/cpu.*/usage": "86,87,245",
+                            "cpu/cpu0/usage": "86,87,245",
+                            "cpu/cpu1/usage": "204,86,245",
+                            "cpu/cpu2/usage": "245,86,167",
+                            "cpu/cpu3/usage": "245,125,86",
+                            "cpu/cpu4/usage": "245,244,86",
+                            "cpu/cpu5/usage": "127,245,86",
+                            "cpu/cpu6/usage": "86,245,164",
+                            "cpu/cpu7/usage": "86,206,245"
+                        },
+                        "/Configuration/Sensors": {
+                            "highPrioritySensorIds": "[\"cpu/cpu.*/usage\"]",
+                            "totalSensors": "[\"cpu/all/usage\"]"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemmonitor.cpucore"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
                             "PreloadWeight": "52"
                         }
                     },
@@ -96,30 +128,26 @@ var layout = {
                             "immutability": "1"
                         },
                         "/Configuration": {
-                            "PreloadWeight": "72"
-                        },
-                        "/Configuration/Appearance": {
-                            "displayTimezoneAsCode": "false",
-                            "spinboxHorizontalPercentage": "60",
-                            "use24hFormat": "2"
-                        },
-                        "/Configuration/ConfigDialog": {
-                            "DialogHeight": "540",
-                            "DialogWidth": "720"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.splitdigitalclock"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration": {
                             "PreloadWeight": "52"
                         }
                     },
                     "plugin": "org.kde.milou"
+                },
+                {
+                    "config": {
+                        "/Configuration": {
+                            "popupHeight": "451",
+                            "popupWidth": "560"
+                        },
+                        "/Configuration/Appearance": {
+                            "showDate": "false"
+                        },
+                        "/Configuration/ConfigDialog": {
+                            "DialogHeight": "631",
+                            "DialogWidth": "810"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
                 }
             ],
             "config": {
@@ -142,6 +170,33 @@ var layout = {
             "location": "top",
             "maximumLength": 75.88888888888889,
             "minimumLength": 75.88888888888889,
+            "offset": 0
+        },
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "3",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2.0,
+            "hiding": "dodge-windows",
+            "location": "left",
+            "maximumLength": 50.94444444444444,
+            "minimumLength": 34.833333333333336,
             "offset": 0
         }
     ],
